f5f05a6149a4b9f8ba6e48c060b3d03bda30c7fc
TH3 F9M0US B0Y ARAV H3R3 <153765611+rahilpanchal@users.noreply.github.com>
2025-07-26 22:11:17 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
   SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  OWNER_NUMBER: "917017659124", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
